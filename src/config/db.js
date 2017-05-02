36adb02a5a293cf86ba69e28e5c588503db4df15
Koen Castermans <castermanskoen@msn.com>
2017-05-02 14:02:08 +0200

    changed promise library for mongoose

--- src/config/db.js
+++ src/config/db.js
@@ -1,6 +1,10 @@
 // Importeren van mongoose
 const mongoose = require('mongoose')
 
+// Mongoose eigen promise library is deprecated
+// Gebruik de ES6 promises
+mongoose.Promise = global.Promise;
+
 // Gebruik mongoose om connectie te maken met de database
 const db = mongoose.connect('mongodb://localhost/flows', (err) => {
     err ?
