bf4640900dbfb660e9f589cc65e00b1c008fada2
Koen Castermans <castermanskoen@msn.com>
2017-05-03 10:19:19 +0200

    changed model for new db config file

--- src/model/flows.js
+++ src/model/flows.js
@@ -1,12 +1,13 @@
 // Importeer het database configuratie bestand
 const db = require('../config/db')
+const mongoose = db.getConnection()
 
 // ObjectId type uit Mongoose halen voor eigen gebruik
-const schema = db.Schema
+const schema = mongoose.Schema
 const ObjectId = schema.ObjectId
 
 // Gebruik db connectie om een nieuw mongoose model aan te maken
-const Flow = db.model('Flow', {
+const Flow = mongoose.model('Flow', {
     trashed: Boolean,
     name: String,
     flow_group_id: ObjectId,
