ccd39d1c404f49cea1ff7c4159e2d0276ecbf3e8
Koen Castermans <castermanskoen@msn.com>
2017-05-03 10:17:27 +0200

    changed db configuration file for test compatability

--- src/config/db.js
+++ src/config/db.js
@@ -1,16 +1,33 @@
 // Importeren van mongoose
 const mongoose = require('mongoose')
 
+// Variabele voor db
+let db
+
 // Mongoose eigen promise library is deprecated
 // Gebruik de ES6 promises
 mongoose.Promise = global.Promise
 
-// Gebruik mongoose om connectie te maken met de database
-const db = mongoose.connect('mongodb://localhost/flows', (err) => {
-    err ?
-        console.log('Can\'t connect to MongoDB', err) :
-        console.log('Connected to MongoDB')
-})
+// Methode om connectie te maken als app in productie is
+// of om gewoon mongoose terug te geven voor tijdens het testen
+exports.connect = (isProduction, done) => {
+    // In productie dus maak connectie met gewone database
+    if (isProduction) {
+        // Gebruik mongoose om connectie te maken met de database
+        db = mongoose.connect('mongodb://localhost/flows', (err) => {
+            err ?
+                done(new Error(`Can't connect to MongoDB`)) :
+                done()
+        })
+    // Niet in productie geef gewoon standaard mongoose module terug
+    // Kan ook vervangen worden met een connectie naar een test database
+    } else {
+        db = mongoose
+        done()
+    }
+}
 
-// Exporteer de connectie met de database voor gebruik in het model
-module.exports = db
+// Methode om connectie op te vragen
+exports.getConnection = () => {
+    return db
+}
